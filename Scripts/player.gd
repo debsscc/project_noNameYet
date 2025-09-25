205ac26fc54f707c0c1c7b80ec800edb76c0bd8a
Debora Carvalho <dbndy3@gmail.com>
2025-09-25 00:18:09 -0300

    🔨: Jump gravity of player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,8 @@ class_name player
 @onready var sprite_animado: AnimatedSprite2D = $animation_player
 @onready var shoot_point = $shoot_point
 @onready var shoot_point_position_x = shoot_point.position.x
+@onready var forca_pulo = 300.0
+@export var gravidade = 600.0
 
 # Variáveis de controle
 var pode_interagir = true
@@ -35,13 +37,22 @@ signal interagir(dono)
 
 func _physics_process(delta: float) -> void:
 	if morto:
-		return 
+		return
+	
+	# Gravidade
+	if not is_on_floor():
+		velocity.y += gravidade * delta
+	else:
+		# Permite pular só se estiver no chão
+		if Input.is_action_just_pressed("jump") and pode_se_mover:
+			velocity.y = -forca_pulo  # valor negativo porque sobe
+	
 	# Pausa movimentação durante ações
 	if esta_atirando:
-		velocity = Vector2.ZERO
-		move_and_slide()  
+		velocity.x = 0
+		move_and_slide()
 		return
-		
+	
 	# Verifica inputs de correr
 	esta_correndo = (
 		Input.is_action_pressed("walk_direita") or
@@ -50,27 +61,26 @@ func _physics_process(delta: float) -> void:
 		Input.is_action_pressed("running_esquerda")
 	)
 	
-	# Movimentação normal
+	# Movimento lateral
 	var direcao_x = Input.get_axis("walk_esquerda", "walk_direita")
-	var direcao = Vector2(direcao_x, 0)
-	
 	var velocidade_atual = velocidade * (multiplicador_corrida if esta_correndo else 1.0)
-
-	if direcao != Vector2.ZERO and pode_se_mover:
-		velocity = direcao.normalized() * velocidade_atual
-		sprite_animado.play("run")
-		
-		if direcao.x < 0:
-			sprite_animado.flip_h = true
-			shoot_point.position.x = -shoot_point_position_x
-		elif direcao.x > 0:
-			sprite_animado.flip_h = false
-			shoot_point.position.x = shoot_point_position_x
+	
+	if pode_se_mover:
+		velocity.x = direcao_x * velocidade_atual
 	else:
-		velocity = Vector2.ZERO
-		sprite_animado.play("idle")
+		velocity.x = 0
+	
+	# Animações
+	if is_on_floor():
+		if velocity.x != 0:
+			sprite_animado.play("run")
+		else:
+			sprite_animado.play("idle")
+	else:
+		sprite_animado.play("jump")  
 	
 	move_and_slide()
+
 
 func dano_jogador():
 	vida_atual -= 10
